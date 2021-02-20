c8425cd339c48a0a999585a787a5617db0cbe24a
Diego <dmartinm1995@gmail.com>
2021-02-19 18:59:44 -0800

    Custom exceptions and catching order

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include <iostream>
 using namespace std;
 
@@ -7,6 +8,26 @@ using namespace std;
 - Exceptions get thrown all the way out of what we call "Call Stack"
 - Exceptions can also be thrown from constructors in classes, e.g: when trying to allocate too much memory on a char *
 */
+
+/* Exception catching order
+- Must be careful to catch your exceptions with your subclasses before any of your parent classes otherwise they are useless 
+- Catch subclasses before the parent classes because of polymorphism 
+*/
+
+// Custom exception inherited from exception class
+class MyException : public exception {
+public:
+    virtual const char *what() const throw() {
+        return "Something bad happened!";
+    }
+};
+
+class Test {
+public:
+    void goesWrong() {
+        throw MyException();
+    }
+};
 
 void mightGoWrong() {
     bool error1 = false;
@@ -26,7 +47,37 @@ void usesMightGoWrong() {
     mightGoWrong();
 }
 
+void goesWrong() {
+    bool error1Detected = true;
+    bool error2Detected = false;
+
+    if (error1Detected) {
+        throw bad_alloc();
+    }
+
+    if (error2Detected) {
+        throw exception();
+    }
+}
+
 int main() {
+
+    try {
+        goesWrong();
+    } catch (bad_alloc &e) {
+        cout << "Catching bad_alloc: " << e.what() << endl;
+    } catch (exception &e) {
+        cout << "Catching exception: " << e.what() << endl;
+    }
+
+    Test test;
+
+    try {
+        test.goesWrong();
+    } catch (MyException &e) {
+        cout << e.what() << endl;
+    }
+
     // try-catch block catches the exception thrown by the program
     // allows to gracefully end the program
     try {
