6921444622fb8cd6f7751a3c7e788c90475da02c
Diego <dmartinm1995@gmail.com>
2021-02-21 18:13:53 -0800

    Basic file manipulations

---
+++ files.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/* Using Files:
+- Writing to files
+- Reading to files 
+- Parsing text files
+- Structs and Padding
+- Reading and writing binary files
+*/
+
+int main() {
+
+    return 0;
+}
