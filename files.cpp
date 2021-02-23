10de3eb21bc25ba11dd7eec3942dbbd6da035a29
Diego <dmartinm1995@gmail.com>
2021-02-23 09:59:52 -0800

    Writing text files using fstream library

--- files.cpp
+++ files.cpp
@@ -1,5 +1,5 @@
+#include <fstream>
 #include <iostream>
-
 using namespace std;
 
 /* Using Files:
@@ -11,6 +11,32 @@ using namespace std;
 */
 
 int main() {
+    string outputFileName = "text.txt";
+    string outFileName = "text2.txt";
+
+    ofstream outputFile;
+
+    // Could also declare a file as
+    fstream outFile;
+
+    // Need to specify if it is an input file or output file
+    outFile.open(outFileName, ios::out);
+
+    int numLines = 10;
+
+    outputFile.open(outputFileName);
+
+    if (outputFile.is_open()) {
+        for (int i = 0; i < numLines; i++) {
+            outputFile << i << ". This is line # " << i << "." << endl;
+        }
+        outputFile.close();
+
+    } else {
+        cout << "Could not create file: " << outputFileName << endl;
+    }
+
+    outFile.close();
 
     return 0;
 }
