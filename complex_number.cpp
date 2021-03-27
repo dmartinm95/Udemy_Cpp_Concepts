daa883623aaaaae7d5e3ff9882123dc61aa2eb08
Diego <dmartinm1995@gmail.com>
2021-03-26 19:22:45 -0700

    Plus operator overloading for Complex Numbers Class

--- complex_number.cpp
+++ complex_number.cpp
@@ -46,14 +46,41 @@ ostream &operator<<(ostream &out, const Complex &complex) {
     return out;
 }
 
+// Plus operator overloading
+Complex operator+(const Complex &c1, const Complex &c2) {
+    return Complex(c1.getReal() + c2.getReal(), c1.getImaginary() + c2.getImaginary());
+}
+
+// Plus operator overloading
+Complex operator+(const Complex &c1, double d) {
+    return Complex(c1.getReal() + d, c1.getImaginary());
+}
+
+// Plus operator overloading
+Complex operator+(double d, const Complex &c1) {
+    return Complex(c1.getReal() + d, c1.getImaginary());
+}
+
 int main() {
     Complex c1(2, 3);
+    Complex c2(10, 30);
 
-    Complex c2 = c1;
+    Complex c3 = c1 + c2;
+    cout << c1 << " + " << c2 << ": " << c3 << endl;
 
-    Complex c3;
-    c3 = c2;
-    cout << c1 << ": " << c3 << endl;
+    Complex c4(4, 3);
+    Complex c5 = c4 + 7;
+
+    cout << endl;
+    cout << c5 << endl;
+
+    Complex c6(1, 1);
+
+    cout << 2.14 + c6 << endl;
+
+    cout << endl;
+
+    cout << 7 + c1 + c2 + 8 + 9 + c5 + c6 << endl;
 
     return 0;
 }
