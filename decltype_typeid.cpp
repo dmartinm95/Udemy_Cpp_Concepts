cf88642c1db5e53db744273f108b953d694a5175
Diego <dmartinm1995@gmail.com>
2021-04-24 13:11:02 -0700

    auto keyword with trailing return types

--- decltype_typeid.cpp
+++ decltype_typeid.cpp
@@ -2,21 +2,41 @@
 #include <typeinfo>
 using namespace std;
 
-// Typeid is used where the dynamic type or runtime type information of an object is needed
+template <class T, class S>
+// When using auto, we need more information about the return type, in C++11 we can use trailing return types '->' along with decltype
+auto test(T value1, S value2) -> decltype(value1 + value2) {
+    return value1 + value2;
+}
+
+int get() {
+    return 999;
+}
+
+auto test2() -> decltype(get()) {
+    return get();
+}
 
 int main() {
 
     string value;
 
+    // typeid is used where the dynamic type or runtime type information of an object is needed
     cout << typeid(value).name() << endl;
 
     // decltype extracts the type of the variable and assigns it to a variable
-
     decltype(value) name = "Bob";
 
     cout << typeid(name).name() << endl;
 
     cout << name << endl;
+
+    // using auto keyword
+    auto numb = 7;
+    auto text = "Hello";
+
+    cout << test(5, 6) << endl;
+
+    cout << test2() << endl;
 
     return 0;
 }
