39bd61e2b54625ecb3fdbbcc5d007b736366e02e
Diego <dmartinm1995@gmail.com>
2021-04-24 13:14:54 -0700

    Ranged-based loops using auto keyword

--- decltype_typeid.cpp
+++ decltype_typeid.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <typeinfo>
+#include <vector>
 using namespace std;
 
 template <class T, class S>
@@ -37,6 +38,27 @@ int main() {
     cout << test(5, 6) << endl;
 
     cout << test2() << endl;
+
+    // Range-based loops
+    auto texts = {"one", "two", "three"};
+    for (auto text : texts) {
+        cout << text << endl;
+    }
+
+    vector<int> numbers;
+    numbers.push_back(5);
+    numbers.push_back(6);
+    numbers.push_back(7);
+    numbers.push_back(8);
+
+    for (auto number : numbers) {
+        cout << number << endl;
+    }
+
+    string hello = "Hello";
+    for (auto c : hello) {
+        cout << c << endl;
+    }
 
     return 0;
 }
