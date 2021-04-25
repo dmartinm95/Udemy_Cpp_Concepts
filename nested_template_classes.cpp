d705df4c342642f59fcb7c53ba498f07fa2e087a
Diego <dmartinm1995@gmail.com>
2021-04-25 11:50:22 -0700

    Making ring buffer class iterable

--- nested_template_classes.cpp
+++ nested_template_classes.cpp
@@ -38,32 +38,70 @@ public:
     T &get(int pos) {
         return m_values[pos];
     }
+
+    Iterator begin() {
+        return Iterator(0, *this);
+    }
+
+    Iterator end() {
+        return Iterator(m_size, *this);
+    }
 };
 
 template <class T>
 class Ring<T>::Iterator {
+private:
+    int m_pos;
+    Ring &m_ring;
+
 public:
-    void print() {
-        cout << "Hello from iterator" << endl;
+    Iterator(int pos, Ring &aRing) : m_pos(pos), m_ring(aRing) {
+    }
+
+    // Overloading post-fix increment needs to have argument (no argument is for pre-fix increment operator)
+    Iterator &operator++(int) {
+        m_pos++;
+        return *this;
+    }
+
+    // Overloading the pre-fix operator
+    Iterator &operator++() {
+        m_pos++;
+        return *this;
+    }
+
+    T &operator*() {
+        return m_ring.get(m_pos);
+    }
+
+    bool operator!=(const Iterator &other) const {
+        return m_pos != other.m_pos;
     }
 };
 
 int main() {
-
-    Ring<string>::Iterator it;
-
-    it.print();
 
     Ring<string> textring(3);
 
     textring.add("one");
     textring.add("two");
     textring.add("three");
-    textring.add("four");
 
     for (int i = 0; i < textring.size(); i++) {
         cout << textring.get(i) << endl;
     }
 
+    cout << endl;
+
+    // Making class iterable
+    for (Ring<string>::Iterator it = textring.begin(); it != textring.end(); it++) {
+        cout << *it << endl;
+    }
+
+    cout << endl;
+
+    for (string value : textring) {
+        cout << value << endl;
+    }
     return 0;
 }
