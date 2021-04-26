b07dbbb297f042ec386f4c9509756aafda454de2
Diego <dmartinm1995@gmail.com>
2021-04-26 10:14:42 -0700

    Using capture expressions to capture local variables by value or by reference

--- lambda_expression.cpp
+++ lambda_expression.cpp
@@ -14,6 +14,10 @@ void runDivide(double (*divide)(double a, double b)) {
     cout << divide(20.0, 5.0) << endl;
 }
 
+void runAdd(double (*add)(double a, double b, double c)) {
+    cout << add(4.0, 1000.0, 10.0) << endl;
+}
+
 int main() {
 
     // Lambda expression - sort of a function that has no name
@@ -47,6 +51,40 @@ int main() {
     cout << pDivide(10.0, 0) << endl;
 
     runDivide(pDivide);
+
+    auto pAdd = [](double a, double b, double c) -> double {
+        return a + b + c;
+    };
+
+    cout << pAdd(4.5, 1.5, 2.5) << endl;
+
+    runAdd(pAdd);
+
+    // Lambda capture expressions
+    int one = 1;
+    int two = 2;
+    int three = 3;
+
+    // Capture one and two by value
+    [one, two]() { cout << "HELLO: " << one << ", " << two << endl; }();
+
+    // Capture all local variables by value
+    [=]() { cout << "HELLO: " << one << ", "
+                 << two << ", " << three << endl; }();
+
+    // Capture all local variables by value, but capture three by reference
+    [=, &three]() { 
+        three = 7;
+        cout << "HELLO: " << one << ", "
+                         << two << ", " << three << endl; }();
+
+    // Capture all local variables by reference
+    [&]() { 
+        three = 10;
+        two = 8;
+        one = 9;
+        cout << "HELLO: " << one << ", "
+                         << two << ", " << three << endl; }();
 
     return 0;
 }
