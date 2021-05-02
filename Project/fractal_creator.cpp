7f28012e7a62177c5879baa2bf6d2ae3ce4aadce
Diego <dmartinm1995@gmail.com>
2021-05-01 18:07:18 -0700

    Applied scaling to pixel coordinates to range between [-1, 1]

--- Project/fractal_creator.cpp
+++ Project/fractal_creator.cpp
@@ -12,12 +12,26 @@ int main() {
     // Set a white pixel in the middle of the bitmap
     bitmap.setPixel(WIDTH / 2, HEIGHT / 2, 255, 255, 255);
 
-    // Color the entire bitmap RED
+    double min = 999999;
+    double max = -999999;
+
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            bitmap.setPixel(x, y, 255, 0, 0);
+            // Want to get xFractal to range between [-1, 1] by using offsets and scaling
+            double xFractal = (x - WIDTH / 2) / (WIDTH / 2.0);
+            double yFractal = (y - HEIGHT / 2) / (HEIGHT / 2.0);
+
+            if (xFractal < min) {
+                min = xFractal;
+            }
+
+            if (xFractal > max) {
+                max = xFractal;
+            }
         }
     }
+
+    cout << min << ", " << max << endl;
 
     bitmap.write("test.bmp");
 
